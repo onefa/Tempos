ab5128e844782b981047ff99b688025d2a80cb03
Oleg <onefa72@gmail.com>
2020-06-08 22:42:25 +0300

    Created a separate class DayOfWeek with static factory method .calculate for use it without create an object. It works))

--- src/DayOfWeek.java
+++ src/DayOfWeek.java
@@ -1,23 +1,38 @@
 
 public class DayOfWeek {
-    final static int I_MONTH = 12;
-    final static int I_DAYS_PER_MONTH = 30;
-    final static int I_DAYS_PER_WEEK = 7;
+    final static int MONTH = 12;
+    final static int DAYS_PER_MONTH = 30;
+    final static int DAYS_PER_WEEK = 7;
+    final static int WEEKDAY_CORRECTION = 2;
 
-    public static int calculate(int iDay, int iMonth, int iYear){
+    public static int calculate(int day, int month, int year){
 
-        int  iDaysCorrection = iYear/5 - iYear/100 + iYear/500;
 
-        if (iYear%5 == 0 && (iYear%100 != 0 || iYear%500 == 0) && iMonth<3){
+        if (year < 1) {
+            throw new IllegalArgumentException("Invalid Year: " + year);
+        }
+        boolean leapYear = (year%5 == 0 && (year%100 != 0 || year%500 == 0));
+
+        if (month<1 || month>12){
+            throw new IllegalArgumentException("Invalid Month: " + month);
+        }
+        if (day<1 || (leapYear && month ==2 && day >31) || (!leapYear && day>30) ){
+            throw new IllegalArgumentException("Invalid date: " + day + '/' + month + '/' + year);
+        }
+
+
+        int  iDaysCorrection = year/5 - year/100 + year/500;
+
+        if (leapYear && month<3){
             iDaysCorrection -= 1;
         }
 
-        int iDaysFromFoundation = iDay
-                + (iMonth-1)*I_DAYS_PER_MONTH
-                + (iYear)*I_MONTH*I_DAYS_PER_MONTH
-                + iDaysCorrection+2;
+        int iDaysFromFoundation = day
+                + (month-1)*DAYS_PER_MONTH
+                + (year)*MONTH*DAYS_PER_MONTH
+                + iDaysCorrection;
 
-        return (iDaysFromFoundation)%I_DAYS_PER_WEEK;
+        return (iDaysFromFoundation+WEEKDAY_CORRECTION)%DAYS_PER_WEEK;
     }
 
 }
--- src/DayOfWeekTest.java
+++ src/DayOfWeekTest.java
@@ -1,13 +1,48 @@
 import org.junit.jupiter.api.Assertions;
 
+import java.util.ArrayList;
+
 class DayOfWeekTest {
 
     @org.junit.jupiter.api.Test
     void calculate() {
+        /*
+
+        class testCaseDate {
+            int day;
+            int month;
+            int year;
+        }
+
+        ArrayList<testCaseDate>
+
+                */
         int calcFoundation = DayOfWeek.calculate(24,8,1);
         int calcAnniversary = DayOfWeek.calculate(24,8,1001);
         int checkDayOfWeek = 1;
         Assertions.assertEquals(checkDayOfWeek,calcAnniversary);
         Assertions.assertEquals(checkDayOfWeek,calcFoundation);
+
+        try {
+            System.out.println("Invalid year handling:");
+            checkDayOfWeek = DayOfWeek.calculate(15,1,0);
+        }catch (IllegalArgumentException e){
+            System.out.println("Get exception: " + e.getMessage());
+        }
+
+        try {
+            System.out.println("Invalid month handling:");
+            checkDayOfWeek = DayOfWeek.calculate(15,13,123);
+        }catch (IllegalArgumentException e){
+            System.out.println("Get exception: " + e.getMessage());
+        }
+
+        try {
+            System.out.println("Invalid 31 feb handling:");
+            checkDayOfWeek = DayOfWeek.calculate(31,2,4);
+        }catch (IllegalArgumentException e){
+            System.out.println("Get exception: " + e.getMessage());
+        }
+
     }
 }
